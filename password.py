d7883c92172fec005a8587319d7b6b2db42f4efb
Md Yakub <md100yakub@gmail.com>
2021-11-07 20:40:50 +0530

    Initial Commit

---
+++ password.py
@@ -0,0 +1,46 @@
+import random
+l1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] # small case characters
+l2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] # Capital case characters
+l3 = ['1','2','3','4','5','6','7','8','9','0'] # Numbers
+l4 = ['&','$','@'] # special Characters
+l5 = [] # password
+
+def main():
+    # print(len(l1))
+    i = 0
+    while(i<8):
+        randnum1 = random.randint(0,len(l1)-1)
+        l5.append(l1[randnum1])
+        i+=1
+        randnum2 = random.randint(0,len(l2)-1)
+        l5.append(l2[randnum2])
+        i+=1
+        randnum3 = random.randint(0,len(l3)-1)
+        l5.append(l3[randnum3])
+        i+=1
+        randnum4 = random.randint(0,len(l4)-1)
+        l5.append(l4[randnum4])
+        i+=1
+    # print(l5)
+    password = ''.join(l5)
+    print(f'Your password is : {password}')
+    # Clearing the list
+    for i in range(0,len(l5)):    
+        l5.pop(0)
+        # print(l5)
+
+# Save generated password
+    with open('Generated Password.txt','a') as f:
+        f.write(f'{str(password)}\n')
+
+Generate = 'y'
+while Generate == 'y':
+    Generate = input('Do you want to generate a new password (y/n): ')
+    if Generate == 'y':
+        main()
+    elif Generate == 'n':
+        exit()
+    else:
+        print('Invalid input')
+        print('please enter a valid input')
+        Generate = input('Do you want to generate a new password (y/n): ') # stop the program from exiting due to invalid input
